7bcad0dc63299d2fdde4f485aef7ee619530df02
cleverdj <cleverdj@naver.com>
2020-07-02 17:36:49 +0900

    kops config

---
+++ cluster.sh
@@ -0,0 +1,23 @@
+export KOPS_STATE_STORE="s3://clusters.cloudsec.kr"
+export CLUSTER_NAME="cloudsec.kr"
+ssh-keygen -t rsa -f ../ssh_key/k8s.key -N ''
+export PUBKEY="../ssh_key/k8s.key.pub"
+export NODE_SIZE="t2.medium"
+export MASTER_SIZE="t2.medium"
+
+
+
+kops create cluster \
+--cloud aws \
+--name  $CLUSTER_NAME \
+--state $KOPS_STATE_STORE \
+--topology private \
+--zones ap-northeast-2a,ap-northeast-2c \
+--master-zones ap-northeast-2a \
+--networking calico \
+--bastion \
+--ssh-public-key $PUBKEY \
+--node-count=2 \
+--node-size=$NODE_SIZE \
+--master-size=$MASTER_SIZE \
+--master-count=1
